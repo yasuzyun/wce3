ec31fe1ba6e4a456d20e2b7095230de54c266f6e
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-03-13 11:19:37 +0000

    Initial Commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "64f20233e31944e58831c2331844b890.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
+  
+  
 end
