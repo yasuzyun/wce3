0a8a754036494118f73be4390ebdc2974eaebcec
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-03-14 12:44:21 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  
-  devise_for :admins
-  devise_for :customers
-  root to: "homes#top"
+
+root to: "homes#top"
+ 
+ # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, skip: [:passwords],controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
