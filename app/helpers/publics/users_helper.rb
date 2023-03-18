2489d9811627f9e5bd27b6aca65574995c662226
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-03-18 06:23:17 +0000

    Initial Commit

---
+++ app/helpers/publics/users_helper.rb
@@ -0,0 +1,2 @@
+module Publics::UsersHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,32 @@ devise_for :customers, skip: [:passwords],controllers: {
   sessions: 'public/sessions'
 }
 
+devise_scope :customer do
+    post '/users/show' => 'public/users#show'
+  end
+  
+ scope module: :public do
+    root 'homes#top'
+    get '/about' => 'homes#about'
+    resources :customers,     only: [:show,:create, :edit, :update, :destroy] do
+      collection do
+  	     get 'quit'
+  	     patch 'out'
+  	  end
+  	end  
+  end
+
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
 
-
+namespace :admin do
+    get '/admins' => 'homes#top'
+  	get '/search'=>'search#search'
+    resources :customers,only: [:index,:show,:edit,:update]
+end
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
