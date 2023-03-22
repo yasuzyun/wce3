03dc88667e400356a0a4c15be266d8663faa0023
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-03-22 03:15:51 +0000

    Initial Commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,13 +4,13 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable4
   
-  has_many :addresses, dependent: :destroy
+  #has_many :addresses, dependent: :destroy
   
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :email, uniqueness: true
+  #validates :last_name, presence: true
+  #validates :first_name, presence: true
+  #validates :last_name_kana, presence: true
+  #validates :first_name_kana, presence: true
+  #validates :email, uniqueness: true
   
   
 end
